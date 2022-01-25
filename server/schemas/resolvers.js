75a3ff6dea446188ae87167090b7e74a52ad112a
Hrishikesh Kanabar <hrishikesh_kanabar@yahoo.com>
2022-01-24 23:46:08 -0500

    Added resolvers and auth

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -4,13 +4,17 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
   Query: {
-    me: async (parent, args) => {
-      const userData = await User.findOne({})
-        .select('-__v -password')
-        .populate('thoughts')
-        .populate('friends');
-  
-      return userData;
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('workouts')
+          .populate('friends');
+    
+        return userData;
+      }
+    
+      throw new AuthenticationError('Not logged in');
     },
     workouts: async (parent, { username }) => {
       const params = username ? { username } : {};
