04fcb524db7b9dd5a773db7b1b10813ac8759d5b
nnikhil <nikhil_narendran@hotmail.com>
2022-01-25 19:32:34 -0500

    added header and footer

---
+++ client/src/pages/Home.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <main>
+      <div className="flex-row justify-space-between">
+        <div className="col-12 mb-3">{/* PRINT WORK OUTS */}</div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
