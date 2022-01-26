5eb517d8fd51d56bb71ee9010fd55f04a03aa9ab
Dan <dan.pandolfo@gmail.com>
2022-01-25 20:11:35 -0500

    integrated apollo hooks

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useQuery } from '@apollo/client';
+import { QUERY_WORKOUTS } from '../utils/queries';
 
 const Home = () => {
+  // use useQuery hook to make query request
+  const { loading, data } = useQuery(QUERY_WORKOUTS);
+
+  const workouts = data?.workouts || [];
+  console.log(workouts);
+
   return (
     <main>
-      <div className="flex-row justify-space-between">
-        <div className="col-12 mb-3">{/* PRINT WORK OUTS */}</div>
+      <div className='flex-row justify-space-between'>
+        <div className='col-12 mb-3'>{/* PRINT WORKOUT LIST */}</div>
       </div>
     </main>
   );
