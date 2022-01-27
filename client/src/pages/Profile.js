928eef0852f80925a6aa3457fe8df2a93f3d6a4f
Dan <dan.pandolfo@gmail.com>
2022-01-26 22:02:23 -0500

    added add gym buddy functionality and button

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,9 +1,10 @@
 import React from "react";
-import { useQuery } from '@apollo/client';
+import { useQuery, useMutation } from '@apollo/client';
 import { Redirect, useParams } from 'react-router-dom';
 import Auth from '../utils/auth';
 
 import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { ADD_FRIEND } from '../utils/mutations';
 
 import WorkoutList from '../components/WorkoutList';
 import FriendList from '../components/FriendList';
@@ -17,13 +18,17 @@ const Profile = () => {
   
   const user = data?.me || data?.user || {};
 
+  const [addFriend] = useMutation(ADD_FRIEND);
+
   // redirect to personal profile page if username is the logged-in user's
   if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
     return <Redirect to="/profile" />;
   }
+
   if (loading) {
     return <div>Loading...</div>;
   }
+
   if (!user?.username) {
     return (
       <h4>
@@ -31,12 +36,28 @@ const Profile = () => {
       </h4>
     );
   }
+
+  const handleClick = async () => {
+    try {
+      await addFriend({
+        variables: { id: user._id }
+      });
+    } catch (e) {
+      console.error(e);
+    }
+  }; 
+
   return (
     <div>
       <div className="flex-row mb-3">
       <h2 className="bg-dark text-secondary p-3 display-inline-block">
       Viewing {userParam ? `${user.username}'s` : 'your'} profile.
-    </h2>
+      </h2>
+      {userParam && (
+        <button className="btn ml-auto" onClick={handleClick}>
+          Add Friend
+        </button>
+      )}
       </div>
 
       <div className="flex-row justify-space-between mb-3">
